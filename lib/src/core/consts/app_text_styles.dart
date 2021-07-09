2aa38f4c86faa92671a6b54adfbc17b5ae320114
Mairramer <mairramer.dasilva28@hotmail.com>
2021-07-09 17:52:58 -0300

    app text styles adicionado ao app

---
+++ lib/src/core/consts/app_text_styles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final infoClouds = GoogleFonts.lexendDeca(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static final elevatedButton = GoogleFonts.lexendDeca(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  static final cityText = GoogleFonts.lexendDeca(
+    color: Colors.white70,
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+  static final temperatureInfo = GoogleFonts.lexendDeca(
+    color: Colors.white,
+    fontSize: 14,
+  );
+  static final temperatureText = GoogleFonts.lexendDeca(
+    color: Colors.white,
+    fontSize: 50,
+  );
+  static final titleText = GoogleFonts.lexendDeca(
+    fontSize: 40,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+  static final titleText2 = GoogleFonts.lexendDeca(
+    fontSize: 40,
+    fontWeight: FontWeight.w200,
+    color: Colors.white,
+  );
+  static final searchText = GoogleFonts.lexendDeca(
+    color: Colors.white70,
+    fontSize: 16,
+  );
+  static final searchError = GoogleFonts.lexendDeca(
+    color: Colors.black,
+    fontSize: 50,
+  );
+  static final tryAgain = GoogleFonts.lexendDeca(
+    color: Colors.black,
+    fontSize: 16,
+  );
+  static final whiteText = GoogleFonts.lexendDeca(
+    color: Colors.white,
+  );
+}
