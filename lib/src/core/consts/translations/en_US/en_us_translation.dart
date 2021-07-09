b8caa77184915415b2662f54175a1b9f3f2f6410
Mairramer <mairramer.dasilva28@hotmail.com>
2021-07-09 17:52:36 -0300

    suporte a traduções adicionado com Get

---
+++ lib/src/core/consts/translations/en_US/en_us_translation.dart
@@ -0,0 +1,17 @@
+final Map<String, String> enUs = {
+  'neste_momento': 'Right now',
+  'minima': 'Minimum',
+  'maxima': 'Maximum',
+  'umidade': 'Humidity',
+  'pressao': 'Pressure',
+  'sensacao': 'Feels Like',
+  'visibilidade': 'Visibility',
+  'vento': 'Winds',
+  'nuvem': 'Clouds',
+  'buscar_novamente': 'Search Again',
+  'cidade': 'City',
+  'add_cidade': 'Add your city',
+  'buscar': 'Search',
+  'erro': 'Search error',
+  'tente_novamente': 'Try Again'
+};
---
+++ lib/src/core/consts/translations/pt_BR/pt_br_translation.dart
@@ -0,0 +1,17 @@
+final Map<String, String> ptBR = {
+  'neste_momento': 'Neste momento',
+  'minima': 'Mínima',
+  'maxima': 'Máxima',
+  'umidade': 'Umidade',
+  'pressao': 'Pressão',
+  'sensacao': 'Sensação Térmica',
+  'visibilidade': 'Visibilidade',
+  'vento': 'Ventos',
+  'nuvem': 'Nuvens',
+  'buscar_novamente': 'Buscar Novamente',
+  'cidade': 'Cidade',
+  'add_cidade': 'Adicione sua cidade',
+  'buscar': 'Buscar',
+  'erro': 'Erro ao buscar',
+  'tente_novamente': 'Tente Novamente'
+};
